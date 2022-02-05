0447cdf24c67f9ae778692e4c1321cd74854fe28
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2022-02-05 18:51:17 -0300

    Feita alteracao do model de transacao pois o ORM estava colocando um nome incorreto no nome da tabela.

--- backend/src/models/transacoes.model.ts
+++ backend/src/models/transacoes.model.ts
@@ -2,7 +2,6 @@ import dbConnection from "../connection";
 import { DataTypes } from "sequelize";
 
 const Transacoes = dbConnection.define("transacoes", {
-    tipo: DataTypes.INTEGER,
     data: DataTypes.DATE,
     valor: DataTypes.DOUBLE,
     cpf: DataTypes.STRING,
