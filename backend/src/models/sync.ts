3022ec6bfdbe41a4112ce6e388e3a327df4dc48f
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2022-02-05 19:09:24 -0300

    Feito cadastro dos tipos de transacao no banco servindo de dependencia da aplicacao, sendo obrigatorio o cadastro sempre que a aplicacao subir.

--- backend/src/models/sync.ts
+++ backend/src/models/sync.ts
@@ -1,10 +1,12 @@
 import associations from "./associations";
 import Transacoes from "./transacoes.model";
 import TiposTransacoes from "./tipos-transacoes.model";
+import { EnumTiposTransacao } from "../enums/tipos-transacao.enum";
+import { EnumNatureza } from "../enums/natureza-transacao.enum";
 
 export default async function sync() {
-    const force = true;
-    const alter = true;
+    const force = false;
+    const alter = false;
     const env = process.env.environment;
 
     console.log(`A M B I E N T E: ${env}`);
@@ -18,6 +20,45 @@ export default async function sync() {
 
         try {
             await TiposTransacoes.sync({ force, alter });
+            await TiposTransacoes.bulkCreate([
+                {
+                    descricao: EnumTiposTransacao.DEBITO,
+                    natureza: EnumNatureza.ENTRADA
+                },
+                {
+                    descricao: EnumTiposTransacao.BOLETO,
+                    natureza: EnumNatureza.SAIDA
+                },
+                {
+                    descricao: EnumTiposTransacao.FINANCIAMENTO,
+                    natureza: EnumNatureza.SAIDA
+                },
+                {
+                    descricao: EnumTiposTransacao.CREDITO,
+                    natureza: EnumNatureza.ENTRADA
+                },
+                {
+                    descricao: EnumTiposTransacao.RECEBIMENTOEMPRESTIMO,
+                    natureza: EnumNatureza.ENTRADA
+                },
+                {
+                    descricao: EnumTiposTransacao.VENDAS,
+                    natureza: EnumNatureza.ENTRADA
+                },
+                {
+                    descricao: EnumTiposTransacao.RECEBIMENTOTED,
+                    natureza: EnumNatureza.ENTRADA
+                },
+                {
+                    descricao: EnumTiposTransacao.RECIMENTODOC,
+                    natureza: EnumNatureza.ENTRADA
+                },
+                {
+                    descricao: EnumTiposTransacao.ALUGUEL,
+                    natureza: EnumNatureza.SAIDA
+                }
+            ]);
+
             await Transacoes.sync({ force, alter });
         } catch (error) {
             console.log(`Falha ao sincronizar: ${error}`);
