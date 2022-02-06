e07efa5aaa2904f27b6000addfe930f120c88585
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2022-02-06 16:47:25 -0300

    Feitas mais estilizacoes da tabela e criado array para ser iterado para a criacao da tabela.

--- frontend/src/shared/components/Table/index.tsx
+++ frontend/src/shared/components/Table/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { ITransacao } from "../../interfaces/transacao.interface";
 import {
     CustomTable,
     TableHead,
@@ -8,12 +9,47 @@ import {
     TableColumn
 } from "./styles";
 
-interface ITable {}
+interface ITable {
+    // transacoes: ITransacao[]
+}
+
+const transacoes: ITransacao[] = [
+    {
+        id: 1,
+        tiposTransacaoId: "Recebimento Empréstimo",
+        data: "01/03/2019 15:34:53",
+        valor: "142,00",
+        cpf: "096.206.760-17",
+        cartao: "4753****3153",
+        donoLoja: "MARIA JOSEFINA",
+        nomeLoja: "LOJA DO Ó - MATRIZ"
+    },
+    {
+        id: 2,
+        tiposTransacaoId: "Vendas",
+        data: "113/08/2020 15:32:59",
+        valor: "650,48",
+        cpf: "486.548.821-98",
+        cartao: "4598****0159",
+        donoLoja: "MARIA JOSEFINA",
+        nomeLoja: "LOJA DO Ó - MATRIZ"
+    },
+    {
+        id: 3,
+        tiposTransacaoId: "Recebimento TED",
+        data: "01/06/2019 12:33:33",
+        valor: "506,17",
+        cpf: "232.702.980-56",
+        cartao: "8723****9987",
+        donoLoja: "MARIA JOSEFINA",
+        nomeLoja: "LOJA DO Ó - MATRIZ"
+    }
+];
 
 const Table: FC<ITable> = () => (
     <CustomTable>
         <TableHead>
-            <TableRow>
+            <TableRow index={0}>
                 <TableHeader style={{ textAlign: "left" }}>Tipo</TableHeader>
                 <TableHeader>Data</TableHeader>
                 <TableHeader>Valor (R$)</TableHeader>
@@ -27,46 +63,36 @@ const Table: FC<ITable> = () => (
             </TableRow>
         </TableHead>
         <TableBody>
-            <TableRow>
-                <TableColumn style={{ textAlign: "left" }}>
-                    Recebimento Empréstimo
-                </TableColumn>
-                <TableColumn>01/03/2019</TableColumn>
-                <TableColumn>142,00</TableColumn>
-                <TableColumn>096.206.760-17</TableColumn>
-                <TableColumn>4753****3153</TableColumn>
-                <TableColumn>15:34:53</TableColumn>
-                <TableColumn>MARIA JOSEFINA</TableColumn>
-                <TableColumn style={{ textAlign: "right" }}>
-                    LOJA DO Ó - MATRIZ
-                </TableColumn>
-            </TableRow>
-            <TableRow>
-                <TableColumn style={{ textAlign: "left" }}>Vendas</TableColumn>
-                <TableColumn>113/08/2020</TableColumn>
-                <TableColumn>650,48</TableColumn>
-                <TableColumn>486.548.821-98</TableColumn>
-                <TableColumn>4598****0159</TableColumn>
-                <TableColumn>15:32:59</TableColumn>
-                <TableColumn>MARIA JOSEFINA</TableColumn>
-                <TableColumn style={{ textAlign: "right" }}>
-                    LOJA DO Ó - MATRIZ
-                </TableColumn>
-            </TableRow>
-            <TableRow>
-                <TableColumn style={{ textAlign: "left" }}>
-                    Recebimento TED
-                </TableColumn>
-                <TableColumn>01/06/2019</TableColumn>
-                <TableColumn>506,17</TableColumn>
-                <TableColumn>232.702.980-56</TableColumn>
-                <TableColumn>8723****9987</TableColumn>
-                <TableColumn>12:33:33</TableColumn>
-                <TableColumn>MARIA JOSEFINA</TableColumn>
-                <TableColumn style={{ textAlign: "right" }}>
-                    LOJA DO Ó - MATRIZ
-                </TableColumn>
-            </TableRow>
+            {transacoes.map(
+                (
+                    {
+                        id,
+                        tiposTransacaoId,
+                        data,
+                        valor,
+                        cpf,
+                        cartao,
+                        donoLoja,
+                        nomeLoja
+                    },
+                    index
+                ) => (
+                    <TableRow index={index} key={id}>
+                        <TableColumn style={{ textAlign: "left" }}>
+                            {tiposTransacaoId}
+                        </TableColumn>
+                        <TableColumn>{data}</TableColumn>
+                        <TableColumn>{valor}</TableColumn>
+                        <TableColumn>{cpf}</TableColumn>
+                        <TableColumn>{cartao}</TableColumn>
+                        <TableColumn>{data}</TableColumn>
+                        <TableColumn>{donoLoja}</TableColumn>
+                        <TableColumn style={{ textAlign: "right" }}>
+                            {nomeLoja}
+                        </TableColumn>
+                    </TableRow>
+                )
+            )}
         </TableBody>
     </CustomTable>
 );
--- frontend/src/shared/components/Table/styles/index.ts
+++ frontend/src/shared/components/Table/styles/index.ts
@@ -1,17 +1,26 @@
 import styled from "styled-components";
 
+interface ITableRow {
+    index: number;
+}
+
 export const CustomTable = styled.table`
     width: 100%;
+    border-collapse: collapse;
 `;
 
 export const TableHead = styled.thead``;
 
 export const TableBody = styled.tbody``;
 
-export const TableRow = styled.tr``;
+export const TableRow = styled.tr<ITableRow>`
+    background: ${({ index }) =>
+        index % 2 === 0 ? "white" : "rgb(227, 238, 250)"};
+`;
 
 export const TableHeader = styled.th`
     color: rgb(3, 235, 158);
+    padding: 10px 0;
 `;
 
 export const TableColumn = styled.td`
