15e170746712a5c8600f1d4201199f7b3d7b9402
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2022-02-13 20:58:32 -0300

    Inserida mais uma cor no tema da aplicacao.

--- frontend/src/shared/theme/index.ts
+++ frontend/src/shared/theme/index.ts
@@ -2,7 +2,8 @@ const theme = {
     primary: "rgb(3, 235, 158)",
     secondary: "rgb(227, 238, 250)",
     primaryOpacity: "rgba(3, 235, 158, 0.2)",
-    primaryDark: "#00b67a"
+    primaryDark: "#00b67a",
+    white: "rgb(255, 255, 255)"
 };
 
 export default theme;
