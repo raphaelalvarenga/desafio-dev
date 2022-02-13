d22f5ddc2af97634e772097b557202334cb08463
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2022-02-13 19:27:17 -0300

    Realizadas correcoes de estilizacao do saldo

--- frontend/src/shared/components/Balance/styles/index.ts
+++ frontend/src/shared/components/Balance/styles/index.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    display: none;
     margin: 20px 0 80px 0;
     font-weight: bold;
+
+    @media (min-width: 900px) {
+        display: block;
+    }
 `;
