939f09e7dc51778aabc3c5b72ee8429c9fd917a4
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2022-02-13 22:19:33 -0300

    Corrido bug em Accordion Stories

--- frontend/src/shared/components/Accordion/story/Accordion.stories.tsx
+++ frontend/src/shared/components/Accordion/story/Accordion.stories.tsx
@@ -12,4 +12,22 @@ const Template: ComponentStory<typeof Accordion> = args => (
 
 export const AccordionStory = Template.bind({});
 
-AccordionStory.args = {};
+AccordionStory.args = {
+    transacaoView: {
+        nomeLoja: "Nome da Loja",
+        saldoConta: "2000",
+        registros: [
+            {
+                id: 1,
+                data: "20/02/2022",
+                hora: "14:45:35",
+                valor: "200",
+                cpf: "123.456.789-10",
+                cartao: "1458****7536",
+                donoLoja: "JOÃO MACEDO",
+                nomeLoja: "MACEDO STORE",
+                descricao: "Financiamento"
+            }
+        ]
+    }
+};
